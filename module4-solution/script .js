cabc21e2de96816b6b3471055b71e8b3075bda48
sabero99951-cyber <sabero99951@gmail.com>
2025-12-11 17:37:34 +0200

    Refactor script.js to remove IIFE
    Removed IIFE and retained names array.

--- module4-solution/script .js
+++ module4-solution/script .js
@@ -1,16 +1,2 @@
-(function () {
-
-  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-
-  for (var i = 0; i < names.length; i++) {
-    var name = names[i];
-    var firstLetter = name.charAt(0).toLowerCase();
-
-    if (firstLetter === "j") {
-      byeSpeaker.speak(name);
-    } else {
-      helloSpeaker.speak(name);
-    }
-  }
-
-})();
+// قائمة الأسماء
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
