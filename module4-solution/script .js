83a1a885a02050fd41b270795d950f58210a59cc
sabero99951-cyber <sabero99951@gmail.com>
2025-12-11 17:42:24 +0200

    Implement greeting logic with names array check
    Added an IIFE to check for names array and greet accordingly.

--- module4-solution/script .js
+++ module4-solution/script .js
@@ -1,2 +1,21 @@
-// قائمة الأسماء
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+(function () {
+
+  // تأكد أن المتغير names موجود
+  if (!window.names && typeof names === 'undefined') {
+    console.error('names array not found!');
+    return;
+  }
+
+  for (var i = 0; i < names.length; i++) {
+    var name = names[i];
+    var firstLetter = name.charAt(0).toLowerCase();
+
+    if (firstLetter === "j") {
+      byeSpeaker.speak(name);
+    } else {
+      helloSpeaker.speak(name);
+    }
+  }
+
+})();
+
