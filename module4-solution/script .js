925115a3e08ab66f0973fc184602ef16fe7016fa
sabero99951-cyber <sabero99951@gmail.com>
2025-12-11 18:50:02 +0200

    Update script .js

--- module4-solution/script .js
+++ module4-solution/script .js
@@ -1,21 +1,17 @@
-(function () {
+// هذا هو المحتوى الصحيح والكامل لملف script.js
+// تم دمج مصفوفة الأسماء في هذا الملف وتمت إزالة التغليف الخاطئ (IIFE).
 
-  // تأكد أن المتغير names موجود
-  if (!window.names && typeof names === 'undefined') {
-    console.error('names array not found!');
-    return;
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"]; 
+
+for (var i = 0; i < names.length; i++) {
+  var name = names[i];
+  var firstLetter = name.charAt(0).toLowerCase();
+
+  if (firstLetter === "j") {
+    // يستدعي دالة byeSpeaker المتاحة عالمياً
+    byeSpeaker.speak(name); 
+  } else {
+    // يستدعي دالة helloSpeaker المتاحة عالمياً
+    helloSpeaker.speak(name);
   }
-
-  for (var i = 0; i < names.length; i++) {
-    var name = names[i];
-    var firstLetter = name.charAt(0).toLowerCase();
-
-    if (firstLetter === "j") {
-      byeSpeaker.speak(name);
-    } else {
-      helloSpeaker.speak(name);
-    }
-  }
-
-})();
-
+}
